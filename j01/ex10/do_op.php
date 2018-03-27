781707855b45ec479aec8d105fc28e4b0e8e3286
labElec42 <labelec@mac-labelec-03.42.fr>
2018-03-27 13:25:23 +0200

    OK

---
+++ j01/ex10/do_op.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+$argv[1] = trim($argv[1]);
+$argv[2] = trim($argv[2]);
+$argv[3] = trim($argv[3]);
+if ($argc == 4)
+{
+	if ($argv[2] == "+")
+		echo $argv[1] + $argv[3];
+	if ($argv[2] == "-")
+		echo $argv[1] - $argv[3];
+	if ($argv[2] == "*")
+		echo $argv[1] * $argv[3];
+	if ($argv[2] == "/")
+		echo $argv[1] / $argv[3];
+	if ($argv[2] == "%")
+		echo $argv[1] % $argv[3];
+}
+else
+	echo "Incorrect Parameters";
+echo "\n";
+?>
